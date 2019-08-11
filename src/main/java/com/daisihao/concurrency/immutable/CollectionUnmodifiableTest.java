3baef397c973a15369d8c095b63ec75bab0b4aec
代思豪 <798814330@qq.com>
2019-08-11 11:45:36 +0800

    将普通的Map变成不可以修改的Map
    1、使用Collections.unmodifiableMap修饰的值,后面不可以再修改里面的值

--- src/main/java/com/daisihao/concurrency/immutable/CollectionUnmodifiableTest.java
+++ src/main/java/com/daisihao/concurrency/immutable/CollectionUnmodifiableTest.java
@@ -1,28 +1,24 @@
 package com.daisihao.concurrency.immutable;
 
-import com.daisihao.concurrency.annoations.NotThreadSafe;
 import com.google.common.collect.Maps;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Collections;
 import java.util.Map;
 
-/**
- * 如果使用final修饰基础数据类型,那么是不可以修改的
- * 如果使用final修饰基础引用数据类型,那么不可以将它指向其他对象,但是可以修改里面的值
- * 使用final修饰引用对象,是线程不安全的
- */
 @Slf4j
-@NotThreadSafe
-public class ImmuatbleTest {
+public class CollectionUnmodifiableTest {
 
     private static final Integer a = 1;
     private static final String b = "2";
-    private static final Map<Integer,Integer> map = Maps.newHashMap();
+    private static Map<Integer,Integer> map = Maps.newHashMap();
 
     static {
         map.put(1,2);
         map.put(2,3);
         map.put(3,4);
+        //使用Collections.unmodifiableMap修饰的值,后面不可以再修改里面的值
+        map = Collections.unmodifiableMap(map);
     }
 
     public static void main(String[] args) {
