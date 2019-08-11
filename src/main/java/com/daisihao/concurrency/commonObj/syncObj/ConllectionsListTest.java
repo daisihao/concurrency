20458f106e9532d9e0c14f938a05409b5ca76ac0
代思豪 <798814330@qq.com>
2019-08-11 14:23:06 +0800

    使用Collections.synchronizedList包装的ArrayList是线程安全的类

---
+++ src/main/java/com/daisihao/concurrency/commonObj/syncObj/ConllectionsListTest.java
@@ -0,0 +1,58 @@
+package com.daisihao.concurrency.commonObj.syncObj;
+
+import com.daisihao.concurrency.annoations.NotThreadSafe;
+import com.google.common.collect.Lists;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+@Slf4j
+@NotThreadSafe
+public class ConllectionsListTest {
+
+    //总请求数
+    public static int clientTotal = 5000;
+    //同时并发执行的线程数
+    public static int threadTotal = 200;
+    //这里的成员变量必须new出来
+    public static List<Integer> arrayList = Collections.synchronizedList(Lists.newArrayList());
+
+    //
+    private static void update() {
+        arrayList.add(1);
+    }
+
+    public static void main(String[] args) throws Exception {
+        //定义线程池
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        //定义信号量,容许并发数
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        //计数器
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+
+        for (int i = 0; i < clientTotal; i++) {
+            executorService.execute(() -> {
+                try {
+                    //判断当前线程是否容许被执行
+                    semaphore.acquire();
+                    update();
+                    //释放当前进程
+                    semaphore.release();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                countDownLatch.countDown();
+            });
+        }
+        //保证countDown为0,需要捕捉异常
+        countDownLatch.await();
+        //关闭线程池
+        executorService.shutdown();
+        log.info("ArrayList的长度为：{}", arrayList.size());
+    }
+}
